c00f76a68a6d5fe15272fcda2181739ff47778fa
khraina <84083826+khraina@users.noreply.github.com>
2023-11-11 22:55:44 +0530

    complete

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ root.title("Digital Clock")
 
 
 def time():
-    string= strftime('%H:%M%S %p \n %D')
+    string= strftime('%H:%M:%S %p \n %D')
     label.config(text=string)
     label.after(1000,time)
     
     
-label=tk.Label(root,font=('calibri',50,'bold'),background='aqua blue',foreground='white')
+label=tk.Label(root,font=('calibri',50,'bold'),background='blue',foreground='white')
+label.pack(anchor='center')
+
+time()
+
+
+root.mainloop()
